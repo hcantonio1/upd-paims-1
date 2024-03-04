ca97c5eecaad06e5c090cf43dc848ff197821988
Elaine Lopez <eslopez@up.edu.ph>
2024-03-04 21:46:56 +0800

    .

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,7 +12,9 @@ const firebaseConfig = {
   storageBucket: "react-firebase-v9-b1a6f.appspot.com",
   messagingSenderId: "1089973929613",
   appId: "1:1089973929613:web:bd6cee73d6ed7db292c4c4",
+  databaseURL: "https://react-firebase-v9-b1a6f-default-rtdb.asia-southeast1.firebasedatabase.app",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
