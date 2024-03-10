0e2d0282508c031914b91026034f307c950dc14e
Harold Antonio <hcantonio1@up.edu.ph>
2024-03-10 19:47:08 +0800

    move navbar code from layout.js to navbar.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,15 @@
 import * as React from "react";
 import Navbar from "../components/navbar";
 import Header from "./header";
-import * as styles from "../styles/layout.module.css";
+// import * as styles from "../styles/layout.module.css";
 import {
   makeStyles,
-  Drawer,
   Typography,
-  ListItem,
-  List,
-  ListItemIcon,
-  ListItemText,
   Box,
   Grid,
   GridSpacing,
 } from "@material-ui/core";
-import { HomeRounded, Folder, AddCircleOutline } from "@material-ui/icons";
 // import { useNavigate } from "react-router-dom";
-import { navigate } from "gatsby";
-import RestrictedComponent from "./restrictedComponent";
 
 const navbarWidth = 200;
 
@@ -63,73 +55,11 @@ const useStyles = makeStyles({
 const Layout = ({ pageTitle, children }) => {
   const classes = useStyles(); // FOR STYLING
 
-  // NAVIGATION BAR ITEMS
-  const menuItems = [
-    {
-      text: "Home",
-      icon: <HomeRounded />,
-      path: "/app/home",
-    },
-
-    {
-      text: "Inventory",
-      icon: <Folder />,
-      path: "/app/inventory",
-    },
-
-    {
-      text: "Submit Form",
-      icon: <AddCircleOutline />,
-      path: "/app/submitform",
-    },
-    // {
-    //   text: 'Manage Accounts',
-    //   icon: null,
-    //   path: "/app/manageaccounts/"
-    // }
-
-    // to be added once mafinalize na yung faq page
-    // {
-    //   text: 'FAQ',
-    //   icon: <AddCircleOutlineOutlined color="secondary"/>,
-    //   path: "/create"
-    // },
-  ];
-
   // LAYOUT PROPER
   return (
     // main container
     <Box display="flex" flexDirection="row">
-      {/* NAVIGATION BAR CODE */}
-      <Box>
-        <Drawer
-          className={classes.drawer}
-          variant="permanent"
-          classes={{ paper: classes.drawerPaper }}
-          anchor="left"
-        >
-          <List>
-            {menuItems.map((item) => (
-              <ListItem
-                button
-                key={item.text}
-                onClick={() => navigate(item.path)}
-              >
-                <ListItemIcon>{item.icon}</ListItemIcon>
-
-                <ListItemText primary={item.text} />
-              </ListItem>
-            ))}
-            <RestrictedComponent>
-              <ListItem button onClick={() => navigate("/app/manageaccounts/")}>
-                <ListItemIcon></ListItemIcon>
-
-                <ListItemText primary={"Manage Accounts"} />
-              </ListItem>
-            </RestrictedComponent>
-          </List>
-        </Drawer>
-      </Box>
+      <Navbar />
 
       {/* NON NAVBAR CONTAINER  */}
       <Grid container direction="column">
