f830b84fa31dc2f7334202d292aadf2204ba4793
ajogerio <aaogerio@up.edu.ph>
2024-03-10 15:56:24 +0800

    added initial ui design for home page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -2,11 +2,116 @@ import React from "react";
 import Layout from "../layout";
 // import * as styles from "../../styles/index.module.css";
 import { getUser } from "../../services/auth";
+import { Box, Typography, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    padding: 20,
+  },
+
+  userInfo: {
+    backgroundColor: '#e5e5e5',
+    padding: 20
+  },
+
+  changeLogTextContainer: {
+    backgroundColor: '#e5e5e5',
+    padding: 20,
+  },
+
+  changeLogText: {
+    fontWeight: 'bold'
+  },
+
+  userText: {
+    fontWeight: 'bold'
+  },
+
+  changeLog: {
+    borderStyle: 'solid',
+    borderColor: '#e5e5e5',
+  },
+
+  recentChangesText: {
+    fontStyle: 'italic'
+  }
+})
 
 const HomePage = () => {
+  const classes = useStyles()
+
   return (
     <Layout pageTitle="DASHBOARD">
-      <main>
+      {/* home content container  */}
+      <Box
+        display='flex'
+        flexDirection='column'
+        className={classes.root}
+        sx={{
+          rowGap: 40
+        }}
+      >
+        {/* user information container  */}
+        <Box
+          display='flex'
+          flexDirection='row'
+          className={classes.userInfo}
+        >
+          {/* user icon  */}
+          <Box>
+
+          </Box>
+
+          {/* username  */}
+          <Box>
+            <Typography
+              variant='h6'
+              className={classes.userText}
+            >
+              User:
+            </Typography>
+          </Box>
+
+          <Box>
+            <Typography
+              variant='h6'
+            >
+              Admin (DCS)
+            </Typography>
+          </Box>
+        </Box>
+
+        {/* changelog container  */}
+        <Box
+          display='flex'
+          flexDirection='column'
+        >
+          {/* changelog text  */}
+          <Box
+            className={classes.changeLogTextContainer}
+          >
+            <Typography
+              variant='h6'
+              className={classes.changeLogText}
+            >
+              Changelog
+            </Typography>
+          </Box>
+
+          {/* recent changes container  */}
+          <Box
+            className={classes.changeLog}
+          >
+            <Typography
+              align='center'
+              className={classes.recentChangesText}
+            >
+              No recent changes have been made.
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      {/* <main>
         <div>
           <h1>Hello User!</h1>
           <p>Your profile:</p>
@@ -19,7 +124,7 @@ const HomePage = () => {
           <p>Changelog (tentative):</p>
         </div>
         <div></div>
-      </main>
+      </main> */}
     </Layout>
   );
 };
