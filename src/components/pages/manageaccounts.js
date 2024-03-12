29c5bd4444972af64bb6265a7a8ff4a2d8014c59
Harold Antonio <hcantonio1@up.edu.ph>
2024-03-12 20:09:24 +0800

    created manage accounts page plan

--- src/components/pages/manageaccounts.js
+++ src/components/pages/manageaccounts.js
@@ -1,9 +1,31 @@
 import React from "react";
 import Layout from "../layout";
+import { Box } from "@material-ui/core";
 
 const ManageAccounts = () => (
   <Layout pageTitle="Manage Accounts">
-    <h1>Hello World</h1>
+    <Box>
+      <h1>Inventory Supervisor</h1>
+      <h3>Supervisors, on top of having Encoder access,</h3>
+      <ul>
+        <li>should be able to create and delete Trustee accounts</li>
+        <li>should be able to create and delete Encoder accounts</li>
+      </ul>
+    </Box>
+    <Box>
+      <h1>System Administrator</h1>
+      <h3>The admin</h3>
+      <ul>
+        <li>
+          should be able to create and delete Departments and Inventory
+          Supervisors
+        </li>
+        <li>
+          only has read access, no write or delete, to the properties of the
+          college's departments
+        </li>
+      </ul>
+    </Box>
   </Layout>
 );
 
