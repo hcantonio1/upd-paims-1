d5ff3db2b62b7b62d48390513a8b75064e13045c
Harold Antonio <hcantonio1@up.edu.ph>
2024-03-13 02:31:51 +0800

    correctly handle promises in prefetch with Promise.all

--- src/services/prefetch.js
+++ src/services/prefetch.js
@@ -1,4 +1,4 @@
-import { collection, getDoc, getDocs } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase-config";
 
 const commonCollections = [
@@ -21,27 +21,36 @@ const commonCollections = [
 
 const prefetched = {};
 
-export const fetchCommonData = async () => {
-  commonCollections.forEach(async ({ name, columnNameOfID, valuesToFetch }) => {
-    console.log(`Fetching ${name}`);
-    try {
-      prefetched[name] = {};
-      const collSnap = await getDocs(collection(db, name));
-      collSnap.docs.forEach((doc) => {
-        const data = doc.data();
-        const key = data[columnNameOfID];
-        const values = valuesToFetch.map((column) => {
-          // console.log(column);
-          return data[column];
-        });
-        prefetched[name][key] = values[0];
-        if (name == "item_location") {
-          prefetched[name][key] = `${values[0]} ${values[1]}`;
-        }
+const fetchCollData = async ({ name, columnNameOfID, valuesToFetch }) => {
+  // console.log(`Fetching ${name}`);
+  try {
+    prefetched[name] = {};
+    const collSnap = await getDocs(collection(db, name));
+    collSnap.docs.forEach((doc) => {
+      const data = doc.data();
+      const key = data[columnNameOfID];
+      const values = valuesToFetch.map((column) => {
+        // console.log(column);
+        return data[column];
       });
-    } catch (err) {
-      console.log(`Error fetching ${name}. ${err}`);
-    }
+      prefetched[name][key] = values[0];
+      if (name == "item_location") {
+        prefetched[name][key] = `${values[0]} ${values[1]}`;
+      }
+    });
+  } catch (err) {
+    console.log(`Error fetching ${name}. ${err}`);
+  }
+};
+
+export const fetchCommonData = async () => {
+  const promises = [];
+  commonCollections.forEach(async (coll) => {
+    promises.push(fetchCollData(coll));
   });
-  console.log(prefetched);
+  Promise.all(promises).then(() => {
+    // console.log(prefetched);
+    // console.log(JSON.stringify(prefetched));
+    sessionStorage.setItem("prefetched", JSON.stringify(prefetched));
+  });
 };
