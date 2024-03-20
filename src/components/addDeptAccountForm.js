78008020c6acac9f470054b2a2b55f031a102b70
Harold Antonio <hcantonio1@up.edu.ph>
2024-03-20 22:18:26 +0800

    add form in addDeptAccountFomr.js

--- src/components/addDeptAccountForm.js
+++ src/components/addDeptAccountForm.js
@@ -1,13 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 
 const AddDeptAccountForm = () => {
-  const [collapsed, setCollaped] = useState(true);
+  const [collapsed, setCollapsed] = useState(true);
 
-  return collapsed ? (
-    <Button variant="contained">Add New Account</Button>
-  ) : (
-    <></>
+  const buttonClick = (e) => {
+    setCollapsed(!collapsed);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const form = (
+    <form onSubmit={handleSubmit}>
+      <Box
+        component="container"
+        display="flex"
+        flexDirection="row"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="role" label="Role" variant="standard" />
+      </Box>
+      <Box
+        component="container"
+        display="flex"
+        flexDirection="row"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="email" label="Email" variant="standard" />
+        <TextField id="password" label="Password" variant="standard" />
+        <TextField id="firstname" label="First Name" variant="standard" />
+        <TextField id="lastname" label="Last Name" variant="standard" />
+      </Box>
+      <Button type="submit" variant="contained" size="small" color="success">
+        Submit
+      </Button>
+    </form>
+  );
+
+  return (
+    <>
+      <Button variant="contained" onClick={buttonClick}>
+        {collapsed ? "New Department Account" : "Hide Section"}
+      </Button>
+      {collapsed ? <></> : form}
+    </>
   );
 };
 
