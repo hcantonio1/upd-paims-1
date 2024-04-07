0249628372f5d6a689da6f66093926784de8b3ad
ajogerio <aaogerio@up.edu.ph>
2024-04-07 20:48:34 +0800

    made the text fields of the item details look better using mui TextField and added Date picker. Please npm install @mui/x-date-pickers

--- src/components/selectTextField.js
+++ src/components/selectTextField.js
@@ -2,26 +2,53 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 
-const getFullName = (user) => {
-  return `${user.FirstName} ${user.LastName}`;
-};
-
-function SelectTextField({label, name, value, options, onChange }) {
+function SelectTextField({ label, name, value, options, onChange, getFunc }) {
   return (
     <TextField
       select
+      label={label}
       name={name}
       value={value}
       onChange={onChange}
       sx={{ width: 300 }}
       required
     >
-      {label === "Trustee" && (
+      {label === "Select Trustee" && (
         options.map((option, index) => (
-          <MenuItem 
-          key={`Trustee_${index}`} 
-          value={option.UserID}>
-            {getFullName(option)}
+          <MenuItem
+            key={`Trustee_${index}`}
+            value={option.UserID}>
+            {getFunc(option)}
+          </MenuItem>
+        ))
+      )}
+
+      {label === "Select Category" && (
+        options.map((option, index) => (
+          <MenuItem
+            key={`category_${index}`}
+            value={option.CategoryID}>
+            {option.CategoryName}
+          </MenuItem>
+        ))
+      )}
+
+      {label === "Select Status" && (
+        options.map((option, index) => (
+          <MenuItem
+            key={`status${index}`}
+            value={option.StatusID}>
+            {option.StatusName}
+          </MenuItem>
+        ))
+      )}
+
+      {label === "Select Location" && (
+        options.map((option, index) => (
+          <MenuItem
+            key={`location_${index}`}
+            value={option.LocationID}>
+            {getFunc(option)}
           </MenuItem>
         ))
       )}
