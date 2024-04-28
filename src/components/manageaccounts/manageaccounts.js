e0ce6d9baade97fd770d8de0024e0a13efde37aa
Harold Antonio <hcantonio1@up.edu.ph>
2024-04-28 17:04:45 +0800

    delete DataTable in manageaccounts.js

--- src/components/manageaccounts/manageaccounts.js
+++ src/components/manageaccounts/manageaccounts.js
@@ -6,7 +6,7 @@ import { onSnapshot, collection } from "firebase/firestore";
 import SearchBar from "../common/searchbar.js";
 import FilterBy from "../common/filter.js";
 import AddDeptAccountForm from "./addDeptAccountForm.js";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
 
 const ManageAccounts = () => {
   return (
@@ -25,15 +25,6 @@ export default ManageAccounts;
 const AccountsTable = () => {
   const usersCollection = collection(db, "user");
 
-  // const displayColumns = [
-  //   { key: "UserID", label: "ID", filterable: true },
-  //   { key: "Email", label: "Email", filterable: true },
-  //   { key: "Department", label: "Department", filterable: true },
-  //   { key: "Role", label: "Role", filterable: true },
-  //   { key: "LastName", label: "Last Name", filterable: true },
-  //   { key: "FirstName", label: "First Name", filterable: true },
-  // ];
-
   const displayCol = [
     { field: "UserID", headerName: "ID", width: 90 },
     { field: "Email", headerName: "Email", width: 150 },
@@ -43,9 +34,7 @@ const AccountsTable = () => {
     { field: "FirstName", headerName: "First Name", width: 150 },
   ];
 
-  const filterableColumns = displayCol.filter(
-    (column) => column.filterable
-  );
+  const filterableColumns = displayCol.filter((column) => column.filterable);
   const [accountsData, setAccountsData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [searchResultsEmpty, setSearchResultsEmpty] = useState(false);
@@ -92,36 +81,6 @@ const AccountsTable = () => {
     setSelectedFilter(filter);
   };
 
-  const requestSort = (key) => {
-    let direction = "ascending";
-    if (sortConfig.key === key && sortConfig.direction === "ascending") {
-      direction = "descending";
-    }
-    setSortConfig({ key, direction });
-  };
-
-  const handleSort = (columnKey) => {
-    const direction =
-      columnKey === sortConfig.key && sortConfig.direction === "asc"
-        ? "desc"
-        : "asc";
-    setSortConfig({ key: columnKey, direction });
-    const sortedResults = [...filteredData].sort((a, b) => {
-      const aValue = a[columnKey];
-      const bValue = b[columnKey];
-
-      if (typeof aValue === "number" && typeof bValue === "number") {
-        return direction === "asc" ? aValue - bValue : bValue - aValue;
-      } else if (typeof aValue === "string" && typeof bValue === "string") {
-        return direction === "asc"
-          ? aValue.localeCompare(bValue)
-          : bValue.localeCompare(aValue);
-      }
-      return 0;
-    });
-    setFilteredData(sortedResults);
-  };
-
   return (
     <Box>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
@@ -132,106 +91,28 @@ const AccountsTable = () => {
   />*/}
       </div>
       <div>
-          {searchResultsEmpty ? (
-            <p style={{ textAlign: "center" }}>No records found.</p>
-          ) : (
-            <Box sx={{ height: 400, width: "100%" }}>
-              <DataGrid
-                getRowId={(row) => row.UserID}
-                rows={filteredData.length > 0 ? filteredData : accountsData}
-                columns={displayCol}
-                initialState={{
-                  pagination: {
-                    paginationModel: {
-                      pageSize: 5,
-                    },
-                  },
-                }}
-                pageSizeOptions={[5]}
-                checkboxSelection
-                disableRowSelectionOnClick
-              />
-            </Box>
-          )}
-        </div>
-    </Box>
-  );
-};
-
-
-      {/* <div>
         {searchResultsEmpty ? (
           <p style={{ textAlign: "center" }}>No records found.</p>
         ) : (
-          <DataTable
-            data={accountsData}
-            columns={displayColumns}
-            onSort={handleSort}
-          />
+          <Box sx={{ height: 400, width: "100%" }}>
+            <DataGrid
+              getRowId={(row) => row.UserID}
+              rows={filteredData.length > 0 ? filteredData : accountsData}
+              columns={displayCol}
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 5,
+                  },
+                },
+              }}
+              pageSizeOptions={[5]}
+              checkboxSelection
+              disableRowSelectionOnClick
+            />
+          </Box>
         )}
-      </div> */}
-
-function DataTable({ data, columns, onSort, sortedField }) {
-  // For Layout
-  const tableStyle = {
-    width: "100%",
-    borderCollapse: "collapse",
-    margin: "20px 0",
-    background: "white",
-  };
-
-  const tdStyle = {
-    border: "1px solid #ddd",
-    padding: "8px",
-    textAlign: "left",
-  };
-
-  const renderHeader = (column) => (
-    <th
-      key={column.key}
-      onClick={() => onSort(column.key)}
-      style={{
-        padding: "8px",
-        cursor: "pointer",
-        borderBottom: "2px solid #ccc",
-        backgroundColor: sortedField === column.key ? "#f2f2f2" : "transparent",
-        position: "relative",
-      }}
-    >
-      {column.label}
-      {sortedField === column.key && (
-        <span
-          style={{
-            position: "absolute",
-            top: "50%",
-            right: "8px",
-            transform: "translateY(-50%)",
-          }}
-        >
-          {sortedField === column.key ? "↑" : "↓"}
-        </span>
-      )}
-    </th>
+      </div>
+    </Box>
   );
-
-  return (
-    <div>
-      <table style={tableStyle}>
-        <thead>
-          <tr>{columns.map((column) => renderHeader(column))}</tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              {columns.map((column) => (
-                <td key={column.key} style={tdStyle}>
-                  {item[column.key]}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
+};
