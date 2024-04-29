527426c9a4543e9e2fce0037fb71fa71fa6891e6
Harold Antonio <hcantonio1@up.edu.ph>
2024-04-30 02:10:38 +0800

    add use MUI-x DatePicker with disableFuture  and small size

--- src/components/paimsform/FormDatePicker.js
+++ src/components/paimsform/FormDatePicker.js
@@ -1,14 +1,8 @@
+import { DatePicker } from "@mui/x-date-pickers";
 import React from "react";
 
 const FormDatePicker = (props) => {
-  return (
-    <>
-      <label htmlFor={props.id} style={{ display: "inline-block", verticalAlign: "top" }}>
-        Date Issued
-      </label>
-      <input {...props} type="date" style={{ display: "inline-block" }} />
-    </>
-  );
+  return <DatePicker {...props} inputFormat="YYYY-MM-DD" slotProps={{ textField: { size: "small" } }} disableFuture />;
 };
 
 export default FormDatePicker;
