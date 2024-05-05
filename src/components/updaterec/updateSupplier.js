bd23a860ffb70d4c09561c26a6e464f438169062
Harold Antonio <hcantonio1@up.edu.ph>
2024-05-05 23:40:12 +0800

    create new autofill supplier function

--- src/components/updaterec/updateSupplier.js
+++ src/components/updaterec/updateSupplier.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../../../firebase-config";
+import _ from "lodash";
 
 import { PaimsForm, FormSubheadered, FormRow } from "../paimsform/paimsForm";
 import SmallTextField from "../paimsform/smallTextField";
 import SubmitButton from "../paimsform/submitButton";
-import { autofillSupplierData } from "../../fetchutils/formautofill";
+// import { autofillSupplierData } from "../../fetchutils/formautofill";
+import { fetchSupplierAutofill } from "../../fetchutils/formautofill2";
 
 /* In the future, add a heading "Select Existing Supplier" dropdown component before the first heading, "Supplier Details" */
 
@@ -20,16 +22,64 @@ const UpdateSupplier = () => {
     SupplierName: "",
   });
 
+  const [oldData, setOldData] = useState({});
+  const [formErrors, setFormErrors] = useState({});
+
+  const supplierForUseEffect = formData.SupplierID;
+  useEffect(() => {
+    const autofillSupplierData = async () => {
+      const supplierAutofillData = await fetchSupplierAutofill(formData.SupplierID);
+      if (!!supplierAutofillData) {
+        const supData = supplierAutofillData;
+        setFormData((prevData) => ({
+          ...prevData,
+          City: supData.City,
+          State: supData.State,
+          StreetName: supData.StreetName,
+          SupplierContact: supData.SupplierContact,
+          SupplierName: supData.SupplierName,
+          UnitNumber: parseInt(supData.UnitNumber),
+        }));
+        setOldData(supData);
+      }
+      setOldData({});
+    };
+
+    autofillSupplierData();
+  }, [supplierForUseEffect]);
+
   const handleInputChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
 
-    if (e.target.id === "SupplierID") {
-      autofillSupplierData(e.target.value, setFormData);
-    }
+    // gatherFormErrors();
+    // console.log(formErrors);
   };
+
+  const gatherFormErrors = () => {
+    // SupplierContact: "",
+    // UnitNumber: "",
+    // StreetName: "",
+    // City: "",
+    // State: "",
+    // SupplierID: "",
+    // SupplierName: "",
+    const unchanged = _.isEqual(oldData, formData);
+    console.log(formData, oldData);
+    const contactNumberError = /^\d+$/.test(formData.SupplierContact) ? null : "Numbers only.";
+    const unitNumberError = /^\d+$/.test(formData.UnitNumber) ? null : "Numbers only.";
+    const newErrors = { ...formErrors, unchanged: unchanged, SupplierContact: contactNumberError, UnitNumber: unitNumberError };
+    const filteredErrors = Object.fromEntries(Object.entries(newErrors).filter(([_, value]) => !!!value));
+    console.log("new errors", newErrors);
+    // console.log(contactNumberError);
+    setFormErrors(filteredErrors);
+  };
+
+  // const formHasNoErrors = () => {
+  //   return Object.keys(formErrors) === 0;
+  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
