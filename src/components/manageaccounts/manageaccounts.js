4e9e542ced9edb8a855f965c3c9177fea825efa3
Harold Antonio <hcantonio1@up.edu.ph>
2024-06-25 01:09:41 +0800

    [feature] button for adding new property status

--- src/components/manageaccounts/manageaccounts.js
+++ src/components/manageaccounts/manageaccounts.js
@@ -3,26 +3,31 @@ import Layout from "../common/layout.js";
 import { Box } from "@mui/material";
 import AddDeptAccountForm from "./addDeptAccountForm.js";
 import { DepartmentTable } from "./departmentTable.js";
+import { AddPropStatusForm } from "./addPropStatusForm.js";
 
 const ManageAccounts = () => {
   return (
     <Layout pageTitle="DEPARTMENT">
       <Box sx={{ display: "flex", flexDirection: "column", padding: 2, margin: 1 }}>
-        <DepartmentTable collectionName="user" />
-        <br />
-        <AddDeptAccountForm />
-        <br />
-        <br />
-        <DepartmentTable collectionName="supplier" />
-        <br />
-        <br />
-        <DepartmentTable collectionName="item_location" />
-        <br />
-        <br />
-        <DepartmentTable collectionName="status" />
+        <TableAndButton table={<DepartmentTable collectionName="user" />} button={<AddDeptAccountForm />} />
+        <TableAndButton table={<DepartmentTable collectionName="supplier" />} button={<></>} />
+        <TableAndButton table={<DepartmentTable collectionName="item_location" />} button={<></>} />
+        <TableAndButton table={<DepartmentTable collectionName="status" />} button={<AddPropStatusForm />} />
       </Box>
     </Layout>
   );
 };
 
 export default ManageAccounts;
+
+const TableAndButton = ({ table, button }) => {
+  return (
+    <>
+      {table}
+      <br />
+      {button}
+      <br />
+      <br />
+    </>
+  );
+};
