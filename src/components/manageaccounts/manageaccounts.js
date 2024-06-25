f83532589855185fc22868e1ebe9f3a8abebb042
Harold Antonio <hcantonio1@up.edu.ph>
2024-06-25 09:22:06 +0800

    [change] implement buttons for the rest of the dept tables (supplier, location)

--- src/components/manageaccounts/manageaccounts.js
+++ src/components/manageaccounts/manageaccounts.js
@@ -3,16 +3,17 @@ import Layout from "../common/layout.js";
 import { Box } from "@mui/material";
 import AddDeptAccountForm from "./addDeptAccountForm.js";
 import { DepartmentTable } from "./departmentTable.js";
-import { AddPropStatusForm } from "./addPropStatusForm.js";
+// import { AddPropStatusForm } from "./addPropStatusForm.js";
+import { StatusDeptTableButton, LocationDeptTableButton, SupplierDeptTableButton } from "./deptTableButton.js";
 
 const ManageAccounts = () => {
   return (
     <Layout pageTitle="DEPARTMENT">
       <Box sx={{ display: "flex", flexDirection: "column", padding: 2, margin: 1 }}>
         <TableAndButton table={<DepartmentTable collectionName="user" />} button={<AddDeptAccountForm />} />
-        <TableAndButton table={<DepartmentTable collectionName="supplier" />} button={<></>} />
-        <TableAndButton table={<DepartmentTable collectionName="item_location" />} button={<></>} />
-        <TableAndButton table={<DepartmentTable collectionName="status" />} button={<AddPropStatusForm />} />
+        <TableAndButton table={<DepartmentTable collectionName="supplier" />} button={<SupplierDeptTableButton />} />
+        <TableAndButton table={<DepartmentTable collectionName="item_location" />} button={<LocationDeptTableButton></LocationDeptTableButton>} />
+        <TableAndButton table={<DepartmentTable collectionName="status" />} button={<StatusDeptTableButton />} />
       </Box>
     </Layout>
   );
